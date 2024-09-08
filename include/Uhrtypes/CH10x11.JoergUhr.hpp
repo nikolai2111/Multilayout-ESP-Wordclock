e673c3697e0cf4ce9a7a680f0496705cdfb865c2
Nikolai Zoller <nikolai.zoller@hotmail.com>
2024-09-08 15:22:39 +0200

    Mod: CH10x11.JoergUhr.hpp, mapped the words to the LEDs

--- include/Uhrtypes/CH10x11.JoergUhr.hpp
+++ include/Uhrtypes/CH10x11.JoergUhr.hpp
@@ -35,90 +35,91 @@ public:
         switch (word) {
 
         case FrontWord::es_ist:
-            setFrontMatrixWord(0, 9, 10);
-            setFrontMatrixWord(0, 4, 7);
+            setFrontMatrixWord(0, 9, 10);   // @I
+            setFrontMatrixWord(0, 6, 7);    // ÄS
+            setFrontMatrixWord(0, 1, 4);    // ISCH
             break;
 
-        case FrontWord::nach:
-        case FrontWord::v_nach:
-            setFrontMatrixWord(3, 9, 10);
+        case FrontWord::min_5:
+            setFrontMatrixWord(1, 8, 10);   // FÜF
+            break;
+        
+        case FrontWord::viertel:
+            setFrontMatrixWord(1, 0, 5);    // VIERTU
+            break;
+
+        case FrontWord::min_10:
+            setFrontMatrixWord(2, 8, 10);   // ZÄÄ
+            break;
+
+        case FrontWord::min_20:
+            setFrontMatrixWord(2, 1, 6);    // ZWÄNZG
             break;
 
         case FrontWord::vor:
         case FrontWord::v_vor:
-            setFrontMatrixWord(2, 0, 2);
+            setFrontMatrixWord(3, 8, 10);   // VOR
             break;
 
-        case FrontWord::viertel:
-            setFrontMatrixWord(1, 5, 10);
-            break;
-
-        case FrontWord::min_5:
-            setFrontMatrixWord(0, 0, 2);
-            break;
-
-        case FrontWord::min_10:
-            setFrontMatrixWord(1, 0, 2);
-            break;
-
-        case FrontWord::min_20:
-            setFrontMatrixWord(2, 5, 10);
+        case FrontWord::nach:
+        case FrontWord::v_nach:
+            setFrontMatrixWord(3, 6, 7);    // AB
             break;
 
         case FrontWord::halb:
-            setFrontMatrixWord(3, 3, 7);
+            setFrontMatrixWord(3, 0, 4);    // HAUBI
             break;
 
         case FrontWord::eins:
-            setFrontMatrixWord(4, 8, 10);
+            setFrontMatrixWord(4, 8, 10);   // EIS
             break;
 
-        case FrontWord::hour_1:
-            setFrontMatrixWord(4, 8, 10);
+        case FrontWord::hour_1: // maybe combine with "eins"
+            setFrontMatrixWord(4, 8, 10);   // EIS
             break;
 
         case FrontWord::hour_2:
-            setFrontMatrixWord(4, 4, 7);
+            setFrontMatrixWord(4, 4, 7);    // ZWÖI
             break;
 
         case FrontWord::hour_3:
-            setFrontMatrixWord(4, 0, 2);
+            setFrontMatrixWord(4, 0, 2);    // DRÜ
             break;
 
         case FrontWord::hour_4:
-            setFrontMatrixWord(5, 6, 10);
+            setFrontMatrixWord(5, 6, 10);   // VIERI
             break;
 
         case FrontWord::hour_5:
-            setFrontMatrixWord(5, 2, 5);
+            setFrontMatrixWord(5, 1, 4);    // FÜFI
             break;
 
         case FrontWord::hour_6:
-            setFrontMatrixWord(6, 5, 10);
+            setFrontMatrixWord(6, 5, 10);   // SÄCHSI
             break;
 
         case FrontWord::hour_7:
-            setFrontMatrixWord(6, 0, 4);
+            setFrontMatrixWord(6, 0, 4);    // SIBINI
             break;
 
         case FrontWord::hour_8:
-            setFrontMatrixWord(7, 6, 10);
+            setFrontMatrixWord(7, 6, 10);   // SIBINI
             break;
 
         case FrontWord::hour_9:
-            setFrontMatrixWord(7, 2, 5);
+            setFrontMatrixWord(7, 1, 4);    // NÜNI
             break;
 
         case FrontWord::hour_10:
-            setFrontMatrixWord(8, 7, 10);
+            setFrontMatrixWord(8, 7, 10);   // ZÄNI
             break;
 
         case FrontWord::hour_11:
-            setFrontMatrixWord(8, 0, 3);
+            setFrontMatrixWord(8, 0, 3);    // EUFI
             break;
 
         case FrontWord::hour_12:
-            setFrontMatrixWord(9, 5, 10);
+            setFrontMatrixWord(9, 3, 8);    // ZWÖUFI
             break;
 
         default:
